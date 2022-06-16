9a180ac663730541a7c2332e7d1481bf15f156e5
Max Wenk <max.wenk@protonmail.com>
2022-06-16 12:06:22 +0200

    variable color depth

--- Comic.java
+++ Comic.java
@@ -2,10 +2,12 @@
  * Class with functions for applying a comic-style to a picture.
  * 
  * @author Max Wenk 
- * @version 6/14/2022
+ * @version 6/16/2022
+ * 
+ * Edge exceptions should be updated
  * 
  * Funtions:
- * - convertComicStyle(originalImg, int colorDepth [With power of two]
+ * - convertComicStyle(originalImg, int colorDepth)
  */
 
 //Importing libraries
@@ -16,6 +18,14 @@ import java.util.Random;
 public class Comic {
     //Function for creating a comic style picture
     public Picture convertComicStyle(Picture originalImg, int colorDepth /*With power of two*/) {
+        //For DAU ("dumbest imagineable user") -> so there are no input errors
+        if (colorDepth < 1) {
+            colorDepth = 1; //The program would cause errors if there is a wrong color depth
+        }else if (colorDepth > 255) {
+            colorDepth = 255;
+        }
+        
+        
         Color[][] pixelOld = originalImg.getPixelArray();
         Color[][] pixelNew = new Color[originalImg.getWidth()][originalImg.getHeight()];
         Color colorNew = Color.GREEN;   //Just as a default value
@@ -51,7 +61,44 @@ public class Comic {
     
     //Private funtion for converting the color values to another color depth to achieve a comic effect
     private int color(int color, int colorDepth) {
-        //Converting tthe colors to color ranges
+        //Creating color ranges with a color depth or sth like that
+        int colorDepthSteps = 255 / colorDepth;
+        int[] depthSteps = new int[colorDepth+1];
+        for (int i = 0; i < colorDepth+1; i++) {
+            if (i == 0) {
+                depthSteps[i] = 0;
+                continue;
+            }
+            if (i == colorDepth) {
+                depthSteps[i] = 255;
+                break;
+            }
+            depthSteps[i] = depthSteps[i-1] + colorDepthSteps;   //Could be done with multiplying, but recursion
+        }
+        
+        //Converting the colors to color ranges
+        for (int i = 0; i < depthSteps.length-1; i++) {
+            if (color > depthSteps[colorDepth-1]) {
+                color = 255;
+                break;
+            }
+            if (color < depthSteps[1]) {
+                color = 0;
+                break;
+            }
+            if (color >= depthSteps[i] && color <= depthSteps[i+1]) {
+                color = (int)((depthSteps[i] + depthSteps[i+1]) /2);
+            }
+        }
+        //Just for debugging
+        for (int i = 0; i < depthSteps.length; i++) {
+            //System.out.println(depthSteps[i]);
+            
+        }
+        
+        
+        // Comic filter with a static size
+        /*
         if (color > 200) {
             color = 255;
         }else if (color > 150) {
@@ -63,13 +110,14 @@ public class Comic {
         }else {
             color = 0;
         }
-        
+        */
+       
         //Returning the adjusted color
         return color;
     }
     
     /*
-     * The funtions below are from the Folding the class.
+     * The funtions below are from the Folding class.
      * For better documentation look at that class.
      */
     
@@ -122,7 +170,7 @@ public class Comic {
     
     //Private function for applying a blur
     private Picture blur(Picture originalImg) {
-        double[][] matrix = createMatrixForBlur(15); //Maybe the automatic blur should be used
+        double[][] matrix = createMatrixForBlur((originalImg.getWidth() * originalImg.getHeight()) / 30000); //Just a value for an automatic blur
         return folding(originalImg, matrix);
     }
     //Private function for creating a blur matrix with a given size
